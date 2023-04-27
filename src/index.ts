9eb824513a31ffff9199446106e805f51bda6f51
aiomonitors <sachow168@gmail.com>
2023-04-27 16:06:34 -0400

    Add basic Koa app

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,45 @@
+import Koa from 'koa';
+import Router from '@koa/router';
 import logger from './utils/logger';
 
-logger.info('hi', { hello: true });
+const app = new Koa();
+const router = new Router();
 
-export default '';
+const wait = async (ms: number) =>
+  new Promise<void>((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+
+app.use(async (ctx, next) => {
+  logger.info(`Received request`, {
+    path: ctx.request.path,
+    headers: ctx.request.headers,
+  });
+
+  await next();
+});
+
+app.use(async (ctx, next) => {
+  const start = +new Date();
+  await next();
+
+  logger.info('Sent response', {
+    path: ctx.response.status,
+    body: ctx.response.body,
+    requestTime: +new Date() - start,
+  });
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+router.get('/', async (ctx) => {
+  await wait(1000);
+  ctx.status = 200;
+  ctx.body = 'Hello world';
+});
+
+app.listen(3000, () => {
+  logger.info(`Server started on port ${3000}`);
+});
