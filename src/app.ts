c6c17478abcfb6b9965d53827c4aca45cf278d45
aiomonitors <sachow168@gmail.com>
2023-04-27 19:01:33 -0400

    Create App class

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import { LoggingMiddleware } from './middleware/Logging.middleware';
+import { Version1Router } from './routes/v1';
+
+export class AppServer {
+  app = new Koa();
+
+  v1 = new Version1Router();
+
+  constructor() {
+    this.addExternalMiddleware();
+    this.addLoggingMiddleware();
+    this.setupRouters();
+  }
+
+  addLoggingMiddleware() {
+    this.app.use(LoggingMiddleware.receivedRequestMiddleware);
+    this.app.use(LoggingMiddleware.sentResponseMiddleware);
+  }
+
+  addExternalMiddleware() {
+    this.app.use(bodyParser());
+  }
+
+  setupRouters() {
+    this.app.use(this.v1.setupRoutes().routes());
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,8 @@
-import Koa from 'koa';
-import bodyParser from 'koa-bodyparser';
 import logger from './utils/logger';
-import { LoggingMiddleware } from './middleware/Logging.middleware';
-import { Version1Router } from './routes/v1';
+import { AppServer } from './app';
 
-const app = new Koa();
-const v1 = new Version1Router();
+const app = new AppServer();
 
-const wait = async (ms: number) =>
-  new Promise<void>((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, ms);
-  });
-
-app.use(bodyParser());
-app.use(LoggingMiddleware.receivedRequestMiddleware);
-app.use(LoggingMiddleware.sentResponseMiddleware);
-app.use(v1.setupRoutes().routes());
-
-app.listen(3000, () => {
+app.app.listen(3000, () => {
   logger.info(`Server started on port ${3000}`);
 });
