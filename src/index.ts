fa8ca56c9d2ce68d70984bf4f2e0114a61daa1bb
aiomonitors <sachow168@gmail.com>
2023-04-27 16:27:22 -0400

    Update app

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import Koa from 'koa';
 import Router from '@koa/router';
 import logger from './utils/logger';
+import { LoggingMiddleware } from './middleware/Logging.middleware';
+import { Version1Router } from './routes/v1';
 
 const app = new Koa();
-const router = new Router();
+const v1 = new Version1Router();
 
 const wait = async (ms: number) =>
   new Promise<void>((resolve) => {
@@ -12,33 +14,9 @@ const wait = async (ms: number) =>
     }, ms);
   });
 
-app.use(async (ctx, next) => {
-  logger.info(`Received request`, {
-    path: ctx.request.path,
-    headers: ctx.request.headers,
-  });
-
-  await next();
-});
-
-app.use(async (ctx, next) => {
-  const start = +new Date();
-  await next();
-
-  logger.info('Sent response', {
-    path: ctx.response.status,
-    body: ctx.response.body,
-    requestTime: +new Date() - start,
-  });
-});
-
-app.use(router.routes()).use(router.allowedMethods());
-
-router.get('/', async (ctx) => {
-  await wait(1000);
-  ctx.status = 200;
-  ctx.body = 'Hello world';
-});
+app.use(LoggingMiddleware.receivedRequestMiddleware);
+app.use(LoggingMiddleware.sentResponseMiddleware);
+app.use(v1.setupRoutes().routes());
 
 app.listen(3000, () => {
   logger.info(`Server started on port ${3000}`);
